6884174b589055ca64a3c0d06ad227d31449fe7d
martin-lin-cw <martin6061097@gmail.com>
2024-02-23 15:51:43 +0800

    add aws config, and list buckets

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	godotenv.Load()
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatalf("failed to load configuration, %v", err)
+	}
+
+	s3Client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.UseAccelerate = false
+	})
+
+	response, err := s3Client.ListBuckets(context.Background(), &s3.ListBucketsInput{})
+	if err != nil {
+		fmt.Printf("%+v", err)
+	}
+
+	text, _ := json.Marshal(response)
+	fmt.Printf("%+v", string(text))
 
 }
